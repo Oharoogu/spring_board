b90c34850d14819b589415f7952973a9a621aaea
Oharoogu <chrhwkdrn@naver.com>
2016-01-28 19:05:32 +0900

    SampleController.java - RequestMapping  경로 변경

--- src/main/java/first/sample/controller/SampleController.java
+++ src/main/java/first/sample/controller/SampleController.java
@@ -22,7 +22,7 @@ public class SampleController {
 	@Resource(name="sampleService")
 	private SampleService sampleService;
 	
-	@RequestMapping(value="/sample/openSampleBoardList.do")
+	@RequestMapping(value="/sample/openBoardList.do")
     public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
     	ModelAndView mv = new ModelAndView("/sample/boardList");
     	
@@ -45,4 +45,11 @@ public class SampleController {
 	    }
 	    return mv;
 	}
+	@RequestMapping(value="/sample/openBoardWrite.do")
+	public ModelAndView openBoardWrite(CommandMap commandMap) throws Exception{
+	    ModelAndView mv = new ModelAndView("/sample/boardWrite");
+	     
+	    return mv;
+	}
+	
 }
