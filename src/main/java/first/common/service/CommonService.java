57c41cf6078baf1207dedd15e9d3f30beb9bdc23
Oharoogu <chrhwkdrn@naver.com>
2016-01-29 12:17:08 +0900

    CommonService - selectFileInfo 메서드 추가

--- src/main/java/first/common/service/CommonService.java
+++ src/main/java/first/common/service/CommonService.java
@@ -1,5 +1,7 @@
 package first.common.service;
 
-public interface CommonService {
+import java.util.Map;
 
+public interface CommonService {
+	Map<String, Object> selectFileInfo(Map<String, Object> map) throws Exception;
 }
